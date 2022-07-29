242b66939927dec83bad47a7043342dfa8069899
lluvisolnaomi <110141474+lluvisolnaomi@users.noreply.github.com>
2022-07-29 13:36:56 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    static int SumaNumeros(int num2, int num8, int num12) {
+        int resultado = 2 + 12 + 8;
+        return resultado;
+    }
+
+
+
+    public static void main(String[] args) {
+      int resultado;
+      resultado= SumaNumeros (2,12,8);
+      System.out.println("La suma: "+resultado);
+    }
+}
